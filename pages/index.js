9e5765c2a84e7ecf9623bf1b682cbc46955f25fd
kimts2627 <kimtstststs@gmail.com>
2021-06-24 14:51:08 +0900

    :recycle: Refactor: change request with react-query
    useEffect http request refacor to react-query request

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
 import Table from '../components/Table'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-
-export const reqTodoList = async () => {
-
-  return await axios.get('api/todos')
-}
+import { useQuery, useQueryClient } from 'react-query'
 
 const Home = () => {
-
+  const queryClient = useQueryClient();
   const columns = React.useMemo(
     () => [
       {
@@ -34,37 +30,18 @@ const Home = () => {
     []
   )
 
-  const [data, setData] = useState([]);
-
-  const data1 = React.useMemo(
-    () => [
-      {
-        col1: 'Hello',
-        col2: 'World',
-      },
-      {
-        col1: 'react-table',
-        col2: 'rocks',
-      },
-      {
-        col1: 'whatever',
-        col2: 'you want',
-      },
-    ],
-    []
+  const { data, isLoading, error } = useQuery(
+    'todos', async () => {
+      const result = await axios.get('api/todos');
+      console.log(result)
+      return result.data
+    }, { keepPreviousData: true, staleTime: 5000 }
   )
-
-  useEffect(async () => {
-    const res = await reqTodoList()
-    const data = await res.data
-    setData(data.data);
-    
-  }, [])
 
   return (
     <div className='w-192 h-full border shadow-lg p-5'>
       <h1 className='font-bold text-5xl'>Taesu Kim TODO</h1>
-      <Table data={data} columns={columns} />
+      <Table data={data?.data || []} columns={columns} />
     </div>
   )
 }
